03a08678661454359bc94441fdee5a85a16a7e89
dollyg <dollyg>
2015-11-07 18:11:14 +0530

    striker collijion detection{WIP}

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -11,7 +11,22 @@ var ball={x:0,y:0}
 ball.move =function(){
     this.leftOrRight()
     this.upOrDown()
+
 }
+var Strinker = function(x,y,width,height){
+    this.x=x;
+    this.y=y;
+    this.width =width;
+    this.height = height;
+
+}
+
+var getBottom =function(striker){
+    striker.y + striker.height;
+}
+strikerleft=new Strinker(0,0,30,50);
+strikerright=new Strinker(0,0,30,50);
+
 var board={x:0,y:0,x1:1000,y1:500}
 var events = {
     //LEFT: onLeft(),
@@ -23,12 +38,16 @@ var events = {
 var initGame =function(){
     ball.x = + $_ball.css('x').replace('px','');
     ball.y = + $_ball.css('y').replace('px','');
+    strikerleft = +$_sLeft.css('x').replace('px','');
+    strikerright = +$_sRight.css('x').replace('px','');
     console.log( ball.x , ball.y);
 }
 
 var onPageLoad = function(){
     $_ball =$('.ball');
     $_board =$('.board');
+    $_sLeft =$('.striker_left');
+    $_sRight =$('.striker_right');
     var socket = io.connect(window.location.hostname);
 
     //$(document).keydown(function(e){
@@ -54,21 +73,20 @@ $(onPageLoad);
 var startGame =function(){
 
 
-       if(ball.x == board.x){
+       if(ball.x == board.x || ball.x == strikerleft.x){
            console.log('colides')
           ball.leftOrRight =right;
        }
-        if(ball.x == board.x1){
+        if(ball.x == board.x1 || ball.x == strikerright.x){
             console.log('colides')
             ball.leftOrRight =left;
         }
-        if(ball.y == board.y1){
+        if(ball.y == board.y1 || ball.y==strikerleft.y||ball.y==strikerright.y){
             console.log('colides')
             ball.upOrDown =up;
         }
-        if(ball.y == board.y){
+        if(ball.y == board.y||ball.y==getBottom(strikerleft)||ball.y==getBottom(strikerright)){
             console.log('colides')
-
             ball.upOrDown =down;
         }
 
@@ -99,5 +117,5 @@ var right = function(){
     ball.x++;
 }
 
-ball.leftOrRight= right;
+ball.leftOrRight= left;
 ball.upOrDown =up;
